e7d245b1816e2b7b58890c53e161b608a37f4ca1
Anant Singh <anantsingh.cse@gmail.com>
2023-11-28 15:12:33 +0530

    Added more files

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,18 @@
+const path = require('path');
+
 const express = require('express');
-const {getAddProduct, addProduct} = require('../controllers/product');
+
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
-router.get('/product-form', getAddProduct);
-router.post('/add-product', addProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-module.exports = {router};
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+module.exports = router;
