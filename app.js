feabcf049fb25ea8c007664f2ead102f5c635d81
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2023-12-20 23:19:14 +0530

    Added users

--- app.js
+++ app.js
@@ -18,24 +18,42 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('65832302f3c76206178bde71')
+    .exec()
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
+// ROUTES ----
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
+// CONNECTION ----
 console.log('\nConnecting to MongoDB...')
 mongoose.connect('mongodb+srv://admin-user:mongocluster@notes.mau7f.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
+    return User.findOne().exec()
+  })
+  .then(user => {
+    console.log(user)
+    if(!user){
+      const user = new User({
+        name: 'Anant',
+        email: 'anant.singh@gmail.com',
+        cart: {
+          items: []
+        }
+      });
+      return user.save();
+    }
+  })
+  .then(() => {
     app.listen(3000);
-    console.log('Connection established. Server is running...')
+    console.log('Connection established. Server is running...');
   })
   .catch(err => console.error(err));
