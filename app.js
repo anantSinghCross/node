c06117dada74779412696b2e67e941a9e1ad4aa9
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-01-01 17:04:46 +0530

    Synced uri

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://admin-user:mongocluster@notes.mau7f.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
