55a78be1cd0a829c3b844469d038b82b0501284c
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-01-02 10:56:22 +0530

    Pushed sessions

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const MONGO_URI = 'mongodb+srv://admin-user:mongocluster@notes.mau7f.mongodb.net/shop'
+
+const store = new MongoDBStore({
+  uri: MONGO_URI,
+  collection: 'sessions'
+});
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -20,11 +28,11 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+  session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store })
 );
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('65832302f3c76206178bde71')
     .then(user => {
       req.user = user;
       next();
